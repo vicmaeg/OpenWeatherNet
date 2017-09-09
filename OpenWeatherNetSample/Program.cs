23902781b9c9fd10b40a28d9af2e63469c9e6cfb
Victor Martos <vmartos@fluendo.com>
2017-09-09 13:52:21 +0200

    target .NetStandard 1.6 version instead of 2.0

---
+++ OpenWeatherNetSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenWeatherNetSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
