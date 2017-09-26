ecb57ebcfafb230bfe423a50091d5bfe62a1688c
Victor Martos <vmartos@outlook.com>
2017-09-26 23:02:40 +0200

    WeatherClient changes and add new properties to WeatherClientSettings
    - Add the possibility to instantiate a default WeatherClientSettings.
    - Also add the option to inject a previously created
    WeatherClientSettings

--- OpenWeatherNet/WeatherClient.cs
+++ OpenWeatherNet/WeatherClient.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using OpenWeatherNet.Interfaces;
+using System.Net.Http;
 
 namespace OpenWeatherNet
 {
     public class WeatherClient : IWeatherClient
     {
-        public WeatherClientSettings Settings { get; set; }
+        HttpClient httpClient;
 
-        public ICurrentWeatherClient Current => throw new NotImplementedException();
+        public WeatherClient (string appID) : this (new WeatherClientSettings(appID))
+        {
+        }
 
-        public IForecastClient Forecast => throw new NotImplementedException();
+        public WeatherClient(WeatherClientSettings settings)
+        {
+            Settings = settings;
+            httpClient = new HttpClient ();
+        }
+
+        public WeatherClientSettings Settings { get; }
+
+        public virtual ICurrentWeatherClient Current =>
+            new CurrentWeatherClient(httpClient, Settings);
+
+        public virtual IForecastClient Forecast => throw new NotImplementedException();
+
+        
+
     }
 }
--- OpenWeatherNet/WeatherClientSettings.cs
+++ OpenWeatherNet/WeatherClientSettings.cs
@@ -4,5 +4,12 @@ namespace OpenWeatherNet
 {
     public class WeatherClientSettings
     {
+        public WeatherClientSettings (string appID)
+        {
+            AppId = appID;
+        }
+        public string AppId { get; }
+        public string Version { get; set; } = "2.5";
+        public string BaseURL { get; set; } = "api.openweathermap.org/data/";
     }
 }
