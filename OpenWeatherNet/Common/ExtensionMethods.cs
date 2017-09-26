464b97e390d9a4e1a01e884c7175509d3d413f5e
Victor Martos <vmartos@outlook.com>
2017-09-26 22:58:24 +0200

    Add the JsonConverter class and a Deserializer as an ExtensionMethod

---
+++ OpenWeatherNet/Common/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OpenWeatherNet.Common
+{
+    public static class ExtensionMethods
+    {
+        public static async Task<T> ReadAsAsync<T>(this HttpContent content)
+        {
+            var json = await content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(json, Converter.Settings);
+        }
+    }
+}
