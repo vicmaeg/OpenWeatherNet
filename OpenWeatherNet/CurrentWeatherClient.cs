3ef9c805f5c1f7a137e4f08f29ac112d09b37148
Victor Martos <vmartos@outlook.com>
2017-09-26 23:01:32 +0200

    Add the implementation of ICurrentWeatherClient

---
+++ OpenWeatherNet/CurrentWeatherClient.cs
@@ -0,0 +1,39 @@
+﻿using OpenWeatherNet.Interfaces;
+using System;
+using OpenWeatherNet.Model;
+using System.Threading.Tasks;
+using System.Net.Http;
+using OpenWeatherNet.Common;
+using System.Threading;
+
+namespace OpenWeatherNet
+{
+    public class CurrentWeatherClient : ClientBase, ICurrentWeatherClient
+    {
+        public CurrentWeatherClient(HttpClient client, WeatherClientSettings settings) : base(client, settings)
+        {
+        }
+
+        protected override string ParamURL => "weather";
+
+        public Task<CurrentWeather> GetByCoords(Coord coord, CancellationToken token = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CurrentWeather> GetByID(int cityID, CancellationToken token = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CurrentWeather> GetByName(string cityName, CancellationToken token = default(CancellationToken))
+        {
+            return GetByName<CurrentWeather>(cityName, token);
+        }
+
+        public Task<CurrentWeather> GetByZip(int zipCode, string countryCode, CancellationToken token = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
