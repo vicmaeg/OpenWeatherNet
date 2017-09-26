c137ece3752508b2b5e54245dcb7f442da24b88b
Victor Martos <vmartos@outlook.com>
2017-09-26 23:05:23 +0200

    Rename the sample project to OpenWeatherNet.Sample

---
+++ OpenWeatherNet.Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenWeatherNet;
+using System;
+
+namespace OpenWeatherNet.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherClient client = new WeatherClient("");
+            var weather = client.Current.GetByName("London").Result;
+            Console.WriteLine(weather);
+        }
+    }
+}
