7d41976ad4b9ff30897018b1c25a206abd117fae
Victor Martos <vmartos@fluendo.com>
2017-10-07 11:28:00 +0200

    Show Barcelona current weather in sample program

--- src/OpenWeatherNet.Sample/Program.cs
+++ src/OpenWeatherNet.Sample/Program.cs
@@ -7,6 +7,7 @@ namespace OpenWeatherNet.Sample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Getting Barcelona weather:");
             string apiKey = null;
 
             using (StreamReader sr = File.OpenText("ApiKey.txt"))
@@ -16,7 +17,19 @@ namespace OpenWeatherNet.Sample
             }
             WeatherClient client = new WeatherClient(apiKey);
             var weather = client.Current.GetByName("Barcelona").Result;
-            Console.WriteLine(weather);
+            if (weather.Cod == 200)
+            {
+                Console.WriteLine($"City Name: {weather.Name}");
+                Console.WriteLine($"Coords: lat={weather.Coord.Lat}, lon={weather.Coord.Lon}");
+                Console.WriteLine($"Temperature: {weather.Main.Temp}");
+                Console.WriteLine($"Max Temperature: {weather.Main.TempMax}");
+                Console.WriteLine($"Min Temperature: {weather.Main.TempMin}");
+                Console.WriteLine($"Visibility: {weather.Visibility}");
+                Console.WriteLine($"Wind Speed: {weather.Wind.Speed}");
+                Console.WriteLine($"Wind degrees: {weather.Wind.Deg}");
+            } else {
+                Console.WriteLine("Error obtaining Weather!");
+            }
         }
     }
 }
