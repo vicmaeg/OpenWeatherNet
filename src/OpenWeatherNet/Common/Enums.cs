df220c15e7e6c280d3ee93facd06f6d27b66624b
Victor Martos <vmartos@outlook.com>
2017-10-19 00:43:58 +0200

    Add Enums for Units and Language

---
+++ src/OpenWeatherNet/Common/Enums.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenWeatherNet.Common
+{
+    public enum Units
+    {
+        Standard,
+        Metric,
+        Imperial
+    }
+
+    public enum Language
+    {
+        AR,
+        BG,
+        CA,
+        CZ,
+        DE,
+        EL,
+        EN,
+        FA,
+        FI,
+        FR,
+        GL,
+        HR,
+        HU,
+        IT,
+        JA,
+        KR,
+        LA,
+        LT,
+        MK,
+        NL,
+        PL,
+        PT,
+        RO,
+        RU,
+        SE,
+        SK,
+        SL,
+        ES,
+        TR,
+        UA,
+        VI,
+        ZH_CN,
+        ZH_TW
+    }
+
+
+}
