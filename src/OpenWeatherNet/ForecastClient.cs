d4b479f446ed2a60e7fba1a6b5069fed49f37221
Victor Martos <vmartos@outlook.com>
2017-11-28 22:26:15 +0100

    Add implementation for Forecast endpoint

---
+++ src/OpenWeatherNet/ForecastClient.cs
@@ -0,0 +1,38 @@
+﻿using OpenWeatherNet.Common;
+using OpenWeatherNet.Interfaces;
+using OpenWeatherNet.Model;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace OpenWeatherNet
+{
+    public class ForecastClient : ClientBase, IForecastClient
+    {
+        protected ForecastClient(HttpClient client, WeatherClientSettings settings) : base(client, settings)
+        {
+        }
+
+        protected override string ParamURL => "forecasts";
+
+        public Task<Forecast> GetByCoords(Coord coord, Units units = Units.Standard, Language language = Language.EN, CancellationToken token = default(CancellationToken))
+        {
+            return GetByCoords<Forecast>(coord, units, language, token);
+        }
+
+        public Task<Forecast> GetByID(int cityID, Units units = Units.Standard, Language language = Language.EN, CancellationToken token = default(CancellationToken))
+        {
+            return GetByID<Forecast>(cityID, units, language, token);
+        }
+
+        public Task<Forecast> GetByName(string cityName, Units units = Units.Standard, Language language = Language.EN, CancellationToken token = default(CancellationToken))
+        {
+            return GetByName<Forecast>(cityName, units, language, token);
+        }
+
+        public Task<Forecast> GetByZip(string zipCode, string countryCode, Units units = Units.Standard, Language language = Language.EN, CancellationToken token = default(CancellationToken))
+        {
+            return GetByZip<Forecast>(zipCode, countryCode, units, language, token);
+        }
+    }
+}
--- src/OpenWeatherNet/Interfaces/IForecastClient.cs
+++ src/OpenWeatherNet/Interfaces/IForecastClient.cs
@@ -1,4 +1,5 @@
-﻿using OpenWeatherNet.Model;
+﻿using OpenWeatherNet.Common;
+using OpenWeatherNet.Model;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,9 +7,13 @@ namespace OpenWeatherNet.Interfaces
 {
     public interface IForecastClient
     {
-		Task<Forecast> GetByName(string cityName, CancellationToken token = default(CancellationToken));
-		Task<Forecast> GetByID(int cityID, CancellationToken token = default(CancellationToken));
-		Task<Forecast> GetByCoords(Coord coord, CancellationToken token = default(CancellationToken));
-		Task<Forecast> GetByZip(int zipCode, string countryCode, CancellationToken token = default(CancellationToken));
+		Task<Forecast> GetByName(string cityName, Units units = Units.Standard,
+            Language language = Language.EN, CancellationToken token = default(CancellationToken));
+        Task<Forecast> GetByID(int cityID, Units units = Units.Standard,
+           Language language = Language.EN, CancellationToken token = default(CancellationToken));
+        Task<Forecast> GetByCoords(Coord coord, Units units = Units.Standard,
+            Language language = Language.EN, CancellationToken token = default(CancellationToken));
+        Task<Forecast> GetByZip(string zipCode, string countryCode, Units units = Units.Standard,
+            Language language = Language.EN, CancellationToken token = default(CancellationToken));
     }
 }
